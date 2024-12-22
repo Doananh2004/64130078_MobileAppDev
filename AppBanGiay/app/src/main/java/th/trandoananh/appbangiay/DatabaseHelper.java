22a39df4b34fe542ffb7cfb5fb00bbb4d864204e
Doananh2004 <trandoananhnt@gmail.com>
2024-12-22 14:33:34 +0700

    Tạo lớp hỗ trợ CSDL dự án

---
+++ AppBanGiay/app/src/main/java/th/trandoananh/appbangiay/DatabaseHelper.java
@@ -0,0 +1,63 @@
+package th.trandoananh.appbangiay;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "account.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableQuery = "CREATE TABLE IF NOT EXISTS Accounts " +
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "username TEXT, " +
+                "password TEXT)";
+        db.execSQL(createTableQuery);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Implement migration logic if needed
+    }
+
+    public void insertData(String username, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("username", username);
+        values.put("password", password);
+        db.insert("Accounts", null, values);
+        db.close();
+    }
+
+    // Kiểm tra tài khoản và mật khẩu
+    public boolean checkUser(String username, String password) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = {"id"};
+        String selection = "username=? and password=?";
+        String[] selectionArgs = {username, password};
+        Cursor cursor = db.query("Accounts", columns, selection, selectionArgs,
+                null, null, null);
+        int count = cursor.getCount();
+        cursor.close();
+        db.close();
+
+        return count > 0;
+    }
+
+    // Cập nhật mật khẩu
+    public void updatePassword(String username, String newPassword) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("password", newPassword);
+        db.update("Accounts", values, "username=?", new String[]{username});
+        db.close();
+    }
+}
