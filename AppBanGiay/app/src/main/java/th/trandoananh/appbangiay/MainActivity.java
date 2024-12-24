a424cdc011314c86644342e9afa7fab7347ee025
Doananh2004 <trandoananhnt@gmail.com>
2024-12-24 09:21:08 +0700

    Trang chủ

--- AppBanGiay/app/src/main/java/th/trandoananh/appbangiay/MainActivity.java
+++ AppBanGiay/app/src/main/java/th/trandoananh/appbangiay/MainActivity.java
@@ -1,21 +1,51 @@
 package th.trandoananh.appbangiay;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
+    private final CartFragment cartFragment = new CartFragment();
+    private final HomeFragment homeFragment = new HomeFragment();
+    private final UserFragment userFragment = new UserFragment();
+    private final ChoXacNhanFragment choXacNhanFragment = new ChoXacNhanFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if(itemId == R.id.home){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                    return true;
+                } else if(itemId == R.id.user){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, userFragment).commit();
+                    return true;
+                } else if(itemId == R.id.cart){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, cartFragment).commit();
+                    updateCartBadge();
+                    return true;
+                }else return false;
+            }
+        });
     }
 
     public void updateCartBadge() {
